75fc372bec6340cb0c9a332f1cf7251c2edfe689
JeffreyMeijer <123974@student.drenthecollege.nl>
2019-03-08 08:46:34 +0100

    Studieblok 1 opdracht2.js toegevoegd

---
+++ scripts/opdracht2.js
@@ -0,0 +1,8 @@
+var a = 3;
+var b = 4;
+var c = 5;
+document.getElementById('a+b+c').innerHTML = a+b+c;
+document.getElementById('a+c-b').innerHTML = a+c-b;
+document.getElementById('c/a').innerHTML = c/a;
+document.getElementById('a-c*b').innerHTML = a-c*b;
+document.getElementById('b+c/a').innerHTML = b+c/a;
