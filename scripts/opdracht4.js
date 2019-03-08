7aa9c1beba7b653c530137b9eb2c7144e403135d
JeffreyMeijer <123974@student.drenthecollege.nl>
2019-03-08 08:56:25 +0100

    opdracht 4 switch statement toegevoegd

--- scripts/opdracht4.js
+++ scripts/opdracht4.js
@@ -1,6 +1,7 @@
-document.getElementById("opdracht4").innerHTML = rekenen(5,4,"aftrekken"); 
+document.getElementById("opdracht4if").innerHTML = "Rekenen met if statement => " + rekenenif(5,5,"optellen");
+document.getElementById("opdracht4switch").innerHTML = "Rekenen met switch statement => " + rekenenswitch(5,2,"aftrekken"); 
 
-function rekenen(a,b,actie){
+function rekenenif(a,b,actie){
   if(actie == "optellen"){
     return a + b;
   }
@@ -13,5 +14,20 @@ function rekenen(a,b,actie){
     return a / b;
   }else{
     return "Actie incorrect";
+  }
+}
+function rekenenswitch(a,b,actie){
+  switch (actie) {
+    case "optellen":
+      return a + b;
+      break;
+    case "aftrekken":
+      return a-b;
+    case "vermenigvuldigen":
+      return a * b;
+    case "delen":
+      return a / b;
+    default:
+      return "Actie incorrect";
   }
 }
