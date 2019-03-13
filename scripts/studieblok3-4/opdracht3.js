7ec8856fc8889ab80457d86056c88f5841d7f171
JeffreyMeijer <123974@student.drenthecollege.nl>
2019-03-13 11:17:27 +0100

    Closes #12

---
+++ scripts/studieblok3-4/opdracht3.js
@@ -0,0 +1,21 @@
+var string = "Hello World"
+var vertaald = false;
+function Schuin() {
+  document.getElementById('opdracht3').innerHTML = "";
+  document.getElementById('opdracht3').innerHTML = string.italics();
+}
+function Normaal() {
+  document.getElementById('opdracht3').innerHTML = "";
+  document.getElementById('opdracht3').innerHTML = string;
+}
+function Click(){
+  if(!vertaald){
+    string = "Hallo Wereld";
+    document.getElementById('opdracht3').innerHTML = string;
+    vertaald = true;
+  }else{
+    string = "Hello World";
+    document.getElementById('opdracht3').innerHTML = string;
+    vertaald = false;
+  }
+}
