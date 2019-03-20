1f6dfbf6abca8143319ff0fec2fe68738e0f52c3
JeffreyMeijer <123974@student.drenthecollege.nl>
2019-03-20 10:57:26 +0100

    Closes #24

---
+++ scripts/studieblok6/opdracht2.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $("#changecolor").click(function(){
+    $(".kleurAanpasbaar").css({"color": "blue"});
+  });
+  $("#changeback").click(function(){
+    $(".kleurAanpasbaar").css({"color": "black"});
+  });
+});
